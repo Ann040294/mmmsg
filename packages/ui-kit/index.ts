6dea2dac24c3512fe8ea0511f2895e269fba31c6
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2025-02-11 13:22:32 +0300

    IT-21 add plugins eslint and fix code design

--- packages/ui-kit/index.ts
+++ packages/ui-kit/index.ts
@@ -1,2 +1,2 @@
-export {default as Avatar} from './Avatar/Avatar'
-export {default as Item} from './Item/Item'
+export { default as Avatar } from './Avatar/Avatar';
+export { default as Item } from './Item/Item';
