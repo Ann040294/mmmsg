180250b6b1193c6253e56391241ddb7a3885fd61
gigilya <gigilya1133@gmail.com>
2025-02-17 16:42:33 +0300

    mmmsg-50 add size button, fix height

--- packages/ui-kit/Button/Button.tsx
+++ packages/ui-kit/Button/Button.tsx
@@ -1,21 +1,23 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
-import { Variants } from './types';
+
+import {ButtonSize, ButtonVariants} from './types';
 
 import css from './Button.module.scss';
 
 export interface ButtonProps {
     text: string;
-    variant?: Variants;
+    variant?: ButtonVariants;
+    size?: ButtonSize;
 }
 
 const Button: FC<ButtonProps> = ({
                                      text,
-                                     variant = Variants.PRIMARY,
-                                     ...props
+                                     variant = ButtonVariants.PRIMARY,
+                                     size = ButtonSize.MEDIUM
                                  }) => {
     return (
-        <button className={cn(css.button, css[variant])}>
+        <button className={cn(css.button, css[variant], css[size])}>
             {text}
         </button>
     );
