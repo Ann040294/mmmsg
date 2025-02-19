236222bd6fe1956076f875661c8b2e56aec9d214
gigilya <118998483+gigilya@users.noreply.github.com>
2025-02-19 13:45:16 +0300

    Button: Нет обработки клика у кнопки (#58)
    * mmmsg-49 add onClick Button
    
    * mmmsg-49 modification onClick Button
    
    * mmmsg-49 refactor name onClick

--- packages/ui-kit/Button/Button.tsx
+++ packages/ui-kit/Button/Button.tsx
@@ -1,21 +1,23 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import cn from 'classnames';
-import { Variants } from './types';
+
+import {Variants} from './types';
 
 import css from './Button.module.scss';
 
 export interface ButtonProps {
     text: string;
     variant?: Variants;
+    onClick: () => void;
 }
 
 const Button: FC<ButtonProps> = ({
                                      text,
                                      variant = Variants.PRIMARY,
-                                     ...props
+                                     onClick
                                  }) => {
     return (
-        <button className={cn(css.button, css[variant])}>
+        <button className={cn(css.button, css[variant])} onClick={onClick}>
             {text}
         </button>
     );
