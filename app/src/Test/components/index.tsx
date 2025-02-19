7b8b88071c07f9b48ccec4a0e1a7a2dd74224e2d
Anna <atupikova@mmtr.ru>
2025-02-19 15:41:07 +0300

    42

---
+++ app/src/Test/components/index.tsx
@@ -0,0 +1,79 @@
+import { Layout } from 'ui-kit';
+
+import { Button, ButtonSize, ButtonVariants } from 'ui-kit/Button';
+
+import css from '../style.module.scss';
+
+export const TestButtons = () => {
+    const handleAnyClick = () => alert(42);
+
+    return (
+        <Layout className={css.buttons}>
+            <Button
+                text={'PRIMARY'}
+                variant={ButtonVariants.PRIMARY}
+                size={ButtonSize.SMALL}
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={'PRIMARY'}
+                variant={ButtonVariants.PRIMARY}
+                size={ButtonSize.MEDIUM}
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={'PRIMARY'}
+                variant={ButtonVariants.PRIMARY}
+                size={ButtonSize.LARGE}
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={'PR'}
+                variant={ButtonVariants.PRIMARY}
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={'PRIMARY PRIMARY'}
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={
+                    'PRIMARY PRIMARY PRIMARY PRIMARY PRIMARY PRIMARY PRIMARY PRIMARY'
+                }
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={'SECONDARY'}
+                variant={ButtonVariants.SECONDARY}
+                size={ButtonSize.SMALL}
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={'MEDIUM'}
+                variant={ButtonVariants.SECONDARY}
+                size={ButtonSize.MEDIUM}
+                onClick={handleAnyClick}
+            />
+            <Button
+                text={'LARGE'}
+                variant={ButtonVariants.SECONDARY}
+                size={ButtonSize.LARGE}
+                onClick={handleAnyClick}
+            />
+            <Button
+                isFullWidth={true}
+                text={'isFullWidth={true}'}
+                variant={ButtonVariants.SECONDARY}
+                size={ButtonSize.SMALL}
+                onClick={handleAnyClick}
+            />
+            <Button
+                isFullWidth={false}
+                text={'isFullWidth={false}'}
+                variant={ButtonVariants.SECONDARY}
+                size={ButtonSize.SMALL}
+                onClick={handleAnyClick}
+            />
+        </Layout>
+    );
+};
