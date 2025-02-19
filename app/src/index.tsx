54a5e46d56a3cb7862ecb0902c2f9f2591bce6d3
Иван Фролов <frolov.ia.kst@ad.local>
2025-02-19 12:35:30 +0300

    mmmsg:5 - почистил index

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,59 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Input, InputVariants } from 'ui-kit';
-import SearchOutlined from '@ant-design/icons/SearchOutlined';
-
-import { NoticeTypes } from 'ui-kit/Notice/types';
-
-const App = () => {
-    const [value, setValue] = useState('');
-    const [hasError, setHasError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value);
-    };
-
-    const handleValidate = () => {
-        if (!value.trim()) {
-            setHasError(true);
-            setErrorMessage('This field cannot be empty');
-        } else {
-            setHasError(false);
-            setErrorMessage('');
-        }
-    };
-    console.log(hasError);
-
-    return (
-        <div style={{ padding: 20 }}>
-            <h1>Test Inputs</h1>
-            <Input
-                label={''}
-                variant={InputVariants.FILLED}
-                placeholder="Search..."
-                value={value}
-                iconLeft={SearchOutlined}
-                iconRight={SearchOutlined}
-                onChange={handleChange}
-            />
-            <br />
-            <Input
-                variant={InputVariants.OUTLINED}
-                placeholder="Type your message..."
-                label="Username"
-                value={value}
-                noticeType={hasError ? NoticeTypes.ERROR : undefined}
-                message={errorMessage}
-                onChange={handleChange}
-            />
-            <button onClick={handleValidate}>Validate</button>
-        </div>
-    );
-};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+
     </React.StrictMode>,
 );
