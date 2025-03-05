c67822e5c0e75945e375f9db9c3cf8a2c06a95fb
gigilya <gigilya1133@gmail.com>
2025-03-05 15:38:47 +0300

    mmmsg-13 refactor TextViewer

--- packages/ui-kit/TextViewer/TextViewer.tsx
+++ packages/ui-kit/TextViewer/TextViewer.tsx
@@ -1,25 +1,24 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import cn from 'classnames';
 
-import css from './TextViewer.module.scss'
+import css from './TextViewer.module.scss';
 
 export interface TextViewerProps {
     text: string;
-    isSender?: boolean;
     timestamp: string;
+    isSender?: boolean;
 }
 
 const TextViewer: FC<TextViewerProps> = ({
-                                        text,
-                                        isSender = false,
-                                        timestamp }) => {
+                                             text,
+                                             timestamp,
+                                             isSender = false,
+                                         }) => {
     return (
-        <div className={cn(css.chat)}>
-            <div className={cn(css.chatcontent, {[css.sender]: isSender})}>
-                <p className={cn(css.chattext)}>{text}</p>
-                {timestamp && (
-                    <span className={cn(css.chattimestamp)}>{timestamp}</span>
-                )}
+        <div className={cn(css.message)}>
+            <div className={cn(css.messagecontent, isSender ? [css.sender] : [css.recipient])}>
+                <p className={cn(css.messagetext)}>{text}</p>
+                <span className={cn(css.messagetimestamp)}>{timestamp}</span>
             </div>
         </div>
     );
