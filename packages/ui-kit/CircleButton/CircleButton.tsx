6a38f42383cb5db44966c3511f519df36edaddff
IvanFro1ov <45459668+IvanFro1ov@users.noreply.github.com>
2025-03-07 11:00:07 +0300

    Компонент CircleButton: добавлены состояния, поправлены imports (#84)
    * mmmsg-6: сделал первоначальный вид, но ещё в процессе
    
    * mmmsg:6 - уже сделал хороший вид CircleButton
    
    * mmmsg:6 - сделал правки по review
    
    * mmmsg-6: исправил замечания с review
    
    * mmmsg:6 - поправил import
    
    * mmmsg:6 - заменил import на use
    
    * mmmsg-6: добавил точку с запятой
    
    * fix after review
    
    * fix after review. part 1.1
    
    * исправил наименования импортов
    
    * убрал лишний комент, заменил на абстрактные цвета
    
    ---------
    
    Co-authored-by: Иван Фролов <frolov.ia.kst@ad.local>

---
+++ packages/ui-kit/CircleButton/CircleButton.tsx
@@ -0,0 +1,45 @@
+import { FC, MouseEvent } from 'react';
+import cn from 'classnames';
+
+import { Icon } from '../Icon';
+import { Icon as IconType } from '../Icon/types';
+
+import { CircleButtonSize } from './types';
+
+import css from './CircleButton.module.scss';
+
+interface CircleButtonProps {
+    icon: IconType;
+    isDisabled?: boolean;
+    className?: string;
+    label?: string;
+    size?: CircleButtonSize;
+    onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const CircleButton: FC<CircleButtonProps> = ({
+    icon,
+    label,
+    size = CircleButtonSize.MEDIUM,
+    className,
+    isDisabled,
+    onClick: handleClick,
+}) => {
+    return (
+        <button
+            className={cn(css.button, className)}
+            disabled={isDisabled}
+            onClick={handleClick}
+        >
+            <span className={cn(css.iconWrap, css[size])}>
+                <Icon
+                    icon={icon}
+                    className={css.icon}
+                />
+            </span>
+            {label && <span className={css.label}>{label}</span>}
+        </button>
+    );
+};
+
+export default CircleButton;
