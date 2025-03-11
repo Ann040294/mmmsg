940a43c83d6109aa41aa3bbdbf019eec187b7539
SokSashaa <sokolov.am@mmtr.ru>
2025-03-11 16:49:36 +0300

    mmmsg-14 add input

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -1,4 +1,8 @@
 import { FC } from 'react';
+import SearchOutlined from '@ant-design/icons/SearchOutlined';
+
+import { Input } from 'ui-kit';
+import { InputVariants } from 'ui-kit/Input';
 
 import { ROUTE_CONFIG } from '@shared/config/routeConfig';
 
@@ -7,6 +11,11 @@ const HomePage: FC = () => {
         <>
             <title>{ROUTE_CONFIG.HOME.title}</title>
             <div>Чаты</div>
+            <Input
+                variant={InputVariants.FILLED}
+                placeholder={'Поиск...'}
+                iconLeft={SearchOutlined}
+            />
         </>
     );
 };
