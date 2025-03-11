be1d22a1866952268772034e1c286319992e484c
SokSashaa <sokolov.am@mmtr.ru>
2025-03-11 14:43:07 +0300

    mmmsg-14 add options

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react';
 
 import { ROUTE_CONFIG } from '@shared/config/routeConfig';
+import HeaderPage from '@shared/ui/HeaderPage/HeaderPage';
+
+import { iconsLeftHeaderPage, iconsRightHeaderPage } from './optionsHeaderPage';
 
 const HomePage: FC = () => {
     return (
         <>
             <title>{ROUTE_CONFIG.HOME.title}</title>
-            <div>Чаты</div>
+            <HeaderPage
+                label={ROUTE_CONFIG.HOME.labelPage}
+                iconsRight={iconsRightHeaderPage}
+                iconsLeft={iconsLeftHeaderPage}
+            />
         </>
     );
 };
---
+++ app/src/pages/home/optionsHeaderPage.ts
@@ -0,0 +1,30 @@
+import ArrowLeftOutlined from '@ant-design/icons/ArrowLeftOutlined';
+import FormOutlined from '@ant-design/icons/FormOutlined';
+
+import { IconItemType } from '@shared/ui/HeaderPage/IconItem/types';
+
+export const iconsRightHeaderPage: IconItemType[] = [
+    {
+        id: '1',
+        icon: FormOutlined,
+        onClick: () => {},
+    },
+    {
+        id: '2',
+        icon: ArrowLeftOutlined,
+        onClick: () => {},
+    },
+];
+
+export const iconsLeftHeaderPage: IconItemType[] = [
+    {
+        id: '3',
+        icon: ArrowLeftOutlined,
+        onClick: () => {},
+    },
+    {
+        id: '4',
+        icon: FormOutlined,
+        onClick: () => {},
+    },
+];
