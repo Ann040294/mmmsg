305941d766f886d32d64bf930a780b5faf736c87
SokSashaa <sokolov.am@mmtr.ru>
2025-03-12 15:46:07 +0300

    mmmsg-14 delete file

--- app/src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LayoutPage } from './layout/ui';
-export { default as HomePage } from './home/ui';
-export { default as TestPage } from './test';
-export { default as ProfilePage } from './profile';
