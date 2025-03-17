365a54a04f3f3f758e0dcee85e349999b46b1600
gigilya <gigilya1133@gmail.com>
2025-03-17 15:37:23 +0300

    bugfix/64-65 refactor Button.tsx, types.ts

--- packages/ui-kit/Button/Button.tsx
+++ packages/ui-kit/Button/Button.tsx
@@ -6,27 +6,27 @@ import { ButtonSize, ButtonVariants } from './types';
 import css from './Button.module.scss';
 
 export interface ButtonProps {
-    text: string;
     variant?: ButtonVariants;
     size?: ButtonSize;
+    text: string;
     isFullWidth?: boolean;
     onClick?: () => void;
 }
 
 const Button: FC<ButtonProps> = ({
-                                     text,
-                                     variant = ButtonVariants.PRIMARY,
-                                     size = ButtonSize.MEDIUM,
-                                     isFullWidth = false,
-                                     onClick: handleClick,
-                                 }) => {
+    text,
+    variant = ButtonVariants.PRIMARY,
+    size = ButtonSize.MEDIUM,
+    isFullWidth,
+    onClick: handleClick,
+}) => {
     return (
-        <button className={cn(
-            css.button,
-            css[variant],
-            css[size],
-            { [css.fullWidth]: isFullWidth })}
-                onClick={handleClick}>
+        <button
+            className={cn(css.button, css[variant], css[size], {
+                [css.fullWidth]: isFullWidth,
+            })}
+            onClick={handleClick}
+        >
             {text}
         </button>
     );
