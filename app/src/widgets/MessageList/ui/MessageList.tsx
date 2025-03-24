bfe3d60f091cc478828db27b81f067aeb3d52633
SokSashaa <sokolov.am@mmtr.ru>
2025-03-24 17:27:40 +0300

    mmmsg-121 del {page}

--- app/src/widgets/MessageList/ui/MessageList.tsx
+++ app/src/widgets/MessageList/ui/MessageList.tsx
@@ -96,7 +96,6 @@ const MessageList: FC = () => {
                 iconLeft={SearchOutlined}
                 onChange={handleOnChange}
             />
-            {page}
             <div
                 className={css.cardList}
                 ref={rootElement}
