63ea518d50c00cc27103f289f56012cd4373e80f
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2025-03-27 13:42:27 +0300

    mmmsg-121 rename hook

--- app/src/shared/lib/hooks/useCounter.ts
+++ app/src/shared/lib/hooks/useCounter.ts
@@ -1,14 +1,17 @@
 import { useCallback, useState } from 'react';
 
-export const useIncreaseOrDecrease = (initialState: number = 0) => {
+export const useCounter = (initialState: number = 0) => {
     const [count, setCount] = useState<number>(initialState);
 
     const increase = useCallback(() => setCount((prev) => prev + 1), []);
     const decrease = useCallback(() => setCount((prev) => prev - 1), []);
+    const changeCount = useCallback((value: number) => {
+        setCount(value);
+    }, []);
 
     return {
         count,
-        setCount,
+        changeCount,
         increase,
         decrease,
     };
