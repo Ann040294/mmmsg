d4595afe7f861307a4cff2e780dfb4be07d98bf2
gigilya <gigilya1133@gmail.com>
2025-03-31 16:00:07 +0300

    feature-36 add test data

--- app/src/pages/test/ui/TestActions.tsx
+++ app/src/pages/test/ui/TestActions.tsx
@@ -1,30 +1,63 @@
 import { FC } from 'react';
-import CarTwoTone from '@ant-design/icons/CarTwoTone';
+import EditOutlined from '@ant-design/icons/EditOutlined';
+import PlusCircleOutlined from '@ant-design/icons/PlusCircleOutlined';
 
 import { Actions, Layout } from 'ui-kit';
+import { ActionsPosition } from 'ui-kit/Actions';
 
 export const TestActions: FC = () => {
     const handleClickFake = () => alert(42);
 
+    const actionsArr1 = [
+        { text: '1', icon: EditOutlined },
+        { text: '2', icon: PlusCircleOutlined },
+        { text: '3', icon: PlusCircleOutlined },
+        { text: '4', icon: PlusCircleOutlined },
+        { text: '5', icon: PlusCircleOutlined },
+        { text: '6', icon: PlusCircleOutlined },
+    ];
+
+    const actionsArr2 = [
+        { text: '1', icon: EditOutlined },
+        { text: '2', icon: PlusCircleOutlined },
+        { text: '3', icon: PlusCircleOutlined },
+        { text: '4', icon: PlusCircleOutlined },
+        { text: '5', icon: PlusCircleOutlined },
+        { text: '6', icon: EditOutlined },
+    ];
+    const actionsArr3 = [
+        { text: '1', icon: EditOutlined },
+        { text: '2', icon: PlusCircleOutlined },
+        { text: '3', icon: PlusCircleOutlined },
+        { text: '4', icon: PlusCircleOutlined },
+        { text: '5', icon: PlusCircleOutlined },
+        { text: '6', icon: PlusCircleOutlined },
+        { text: '7', icon: PlusCircleOutlined },
+        { text: '8', icon: PlusCircleOutlined },
+        { text: '9', icon: PlusCircleOutlined },
+        { text: '10', icon: PlusCircleOutlined },
+        { text: '11', icon: PlusCircleOutlined },
+    ];
+
     return (
         <Layout>
             <Actions
-                iconArr={[
-                    { text: '1', icon: CarTwoTone },
-                    { text: '1', icon: CarTwoTone },
-                    { text: '1', icon: CarTwoTone },
-                ]}
+                show={3}
+                actionsArr={actionsArr1}
                 onClick={handleClickFake}
             ></Actions>
-            <Actions iconArr={[{ text: '2', icon: CarTwoTone }]}></Actions>
             <Actions
-                iconArr={[
-                    { text: '2', icon: CarTwoTone },
-                    { text: '2', icon: CarTwoTone },
-                    { text: '2', icon: CarTwoTone },
-                    { text: '2', icon: CarTwoTone },
-                    { text: '2', icon: CarTwoTone },
-                ]}
+                actionsArr={[{ text: '2', icon: PlusCircleOutlined }]}
+                onClick={handleClickFake}
+            ></Actions>
+            <Actions
+                position={ActionsPosition.LEFT}
+                actionsArr={actionsArr2}
+            ></Actions>
+            <Actions
+                show={15}
+                actionsArr={actionsArr3}
+                onClick={handleClickFake}
             ></Actions>
         </Layout>
     );
