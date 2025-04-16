3deacf2e3518364a7db14d551f7b88f826cee0f2
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2025-04-16 15:34:03 +0300

    mmmsg-202 правки из закрытого PR

--- app/src/pages/not-found/ui/index.tsx
+++ app/src/pages/not-found/ui/index.tsx
@@ -12,7 +12,6 @@ const NotFoundPage: FC = () => {
         <div className={css.root}>
             <Filler
                 hasGradient
-                hasLoading={false}
                 icon={StopTwoTone}
                 title={t(ROUTE_CONFIG.NOT_FOUND.labelPage)}
                 description={t(ROUTE_CONFIG.NOT_FOUND.description)}
