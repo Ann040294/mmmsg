b7c01e150eb3f220cd4172abf535de9c2467a1a9
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2025-04-22 21:04:43 +0300

    mmmsg-203 fix import

--- app/src/widgets/MessageList/ui/MessageList.tsx
+++ app/src/widgets/MessageList/ui/MessageList.tsx
@@ -8,7 +8,7 @@ import {
 } from 'react';
 import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
-import lodash from 'lodash';
+import { isUndefined } from 'lodash';
 import SearchOutlined from '@ant-design/icons/SearchOutlined';
 
 import { Card, Input, LinearLoader } from 'ui-kit';
@@ -52,7 +52,7 @@ const MessageList: FC = () => {
     }, [isFetching]);
 
     useEffect(() => {
-        const isNotReadyFetch = lodash.isUndefined(isToggled) || isFetching;
+        const isNotReadyFetch = isUndefined(isToggled) || isFetching;
 
         if (isNotReadyFetch) {
             return;
