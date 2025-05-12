4b8dcbc380ca565074fc28f55c8c8b6b555c2ceb
gigilya <gigilya1133@gmail.com>
2025-05-12 17:44:10 +0300

    feature-35 add Dialog

--- app/src/pages/dialog/ui/index.tsx
+++ app/src/pages/dialog/ui/index.tsx
@@ -3,36 +3,52 @@ import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router';
 import VerticalAlignBottomOutlined from '@ant-design/icons/VerticalAlignBottomOutlined';
 
-import {Input, InputVariants, Layout} from 'ui-kit';
+import { Input, InputVariants } from 'ui-kit';
 
 import { Message, MessageVariant } from '@widgets/Message';
 
 import HeaderChat from '@shared/ui/HeaderChat/HeaderChat';
+
+import css from './DialogPage.module.scss';
+import { ArrowRightOutlined, SmileFilled } from '@ant-design/icons';
 
 const DialogUserPage: FC = () => {
     const { userID } = useParams();
     const { t } = useTranslation();
 
     const actionHeaderChat = [
-        { id: '1', text: 'Скачать историю', icon: VerticalAlignBottomOutlined },
+        {
+            id: '1',
+            text: t('Скачать историю'),
+            icon: VerticalAlignBottomOutlined,
+        },
     ];
 
     return (
         <>
-           <div>
-               <HeaderChat
-                   label={'ID пользователя {userID}'}
-                   actions={actionHeaderChat}
-               />
-               <Layout>
-                   <Message text={'Test'} /><Message text={'Test'} /><Message text={'TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest'} />
-                   <Message
-                       text={'Test'}
-                       variant={MessageVariant.SECONDARY}
-                   />
-               <Input variant={InputVariants.FILLED} />
-               </Layout>
-           </div>
+            <div className={css.root}>
+                <HeaderChat
+                    label={'ID пользователя'}
+                    actions={actionHeaderChat}
+                />
+                <div className={css.messageList}>
+                    <Message
+                        text={'Hi, Welcome!'}
+                        variant={MessageVariant.SECONDARY}
+                    />
+                    <Message
+                        text={'How are you? All OK??'}
+                        variant={MessageVariant.SECONDARY}
+                    />
+                    <Message text={'Hi, All OK!!!!!!!!!'} meta={'sent a few seconds ago'}/>
+                    <Input
+                        variant={InputVariants.FILLED}
+                        iconLeft={SmileFilled}
+                        iconRight={ArrowRightOutlined}
+                        placeholder={'Type your message here'}
+                    />
+                </div>
+            </div>
         </>
     );
 };
