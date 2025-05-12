61dd3336268f7b505a77d9e3ccf3e372c9f1aaa9
gigilya <gigilya1133@gmail.com>
2025-05-12 10:31:45 +0300

    feature/15 add Dialog

--- app/src/pages/dialog/ui/index.tsx
+++ app/src/pages/dialog/ui/index.tsx
@@ -1,10 +1,40 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router';
+import VerticalAlignBottomOutlined from '@ant-design/icons/VerticalAlignBottomOutlined';
+
+import {Input, InputVariants, Layout} from 'ui-kit';
+
+import { Message, MessageVariant } from '@widgets/Message';
+
+import HeaderChat from '@shared/ui/HeaderChat/HeaderChat';
 
 const DialogUserPage: FC = () => {
     const { userID } = useParams();
+    const { t } = useTranslation();
 
-    return <div>ID пользователя {userID}</div>;
+    const actionHeaderChat = [
+        { id: '1', text: 'Скачать историю', icon: VerticalAlignBottomOutlined },
+    ];
+
+    return (
+        <>
+           <div>
+               <HeaderChat
+                   label={'ID пользователя {userID}'}
+                   actions={actionHeaderChat}
+               />
+               <Layout>
+                   <Message text={'Test'} /><Message text={'Test'} /><Message text={'TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest'} />
+                   <Message
+                       text={'Test'}
+                       variant={MessageVariant.SECONDARY}
+                   />
+               <Input variant={InputVariants.FILLED} />
+               </Layout>
+           </div>
+        </>
+    );
 };
 
 export default DialogUserPage;
